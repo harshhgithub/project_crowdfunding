6e01b6351b20b8d83ef5cc59612314ce372ecbc0
Harsh Shukla <you@example.com>
2024-11-30 18:33:08 +0530

    made some changes

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -51,7 +51,7 @@ const CreateCampaign = () => {
         <div className="flex flex-wrap gap-[40px]">
           <FormField 
             labelName="Your Name *"
-            placeholder="John Doe"
+            placeholder="Write your name"
             inputType="text"
             value={form.name}
             handleChange={(e) => handleFormFieldChange('name', e)}
